45d7c8b81a9ea64228fb8d7c63f1497925c1a642
seymurahmadov <s.axmedov99@gmail.com>
2023-02-03 20:25:00 +0400

    Initial commit

---
+++ src/main/java/com/bina/az/binaazdata/BinaAzDataApplication.java
@@ -0,0 +1,13 @@
+package com.bina.az.binaazdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinaAzDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BinaAzDataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bina/az/binaazdata/BinaAzDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bina.az.binaazdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BinaAzDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
